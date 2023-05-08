1edb9ad517c30b4982da399f5443b37fe829be37
MyMallVibes <Brad.MyMallVibes@gmail.com>
2023-05-08 14:02:31 -0400

    Update next.config.js for serving index.html from the root

--- next.config.js
+++ next.config.js
@@ -3,12 +3,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-});
+module.exports = (phase, { defaultConfig }) => {
+  const config = withBundleAnalyzer({
+    ...defaultConfig,
+    poweredByHeader: false,
+    trailingSlash: true,
+    basePath: '',
+    // The starter code loads resources from the `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    reactStrictMode: true,
+  });
+
+  return config;
+};
